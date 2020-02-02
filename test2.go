e9046cd20c6cb37bcaecfb203b47b76b64107766
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-02-02 20:13:11 +0700

    Update test2.go

--- test2.go
+++ test2.go
@@ -11,3 +11,17 @@ func main() {
 	var num2 int
 	fmt.Println("Plus or Minus =")
 	fmt.Scan(&vr1)
+X:
+	fmt.Println("number =")
+	fmt.Println(&num1, &num2)
+	if strings.Contains(vr1, "Plus") == true {
+		plus(num1, num2)
+	} else {
+		goto X
+	}
+	if strings.Contains(vr1, "Minus") == true {
+		minus(num1, num2)
+	} else {
+		goto X
+	}
+}
